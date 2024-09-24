ec1cb860ef59ab618c6438f8a06182953c7475e6
Mostafa Farrag <moah.farag@gmail.com>
2024-09-24 16:05:50 +0200

    add doc string

--- src/voice_for_nature_backend/chat_model.py
+++ src/voice_for_nature_backend/chat_model.py
@@ -1,11 +1,44 @@
 from typing import List
-from llama_index.core import SimpleDirectoryReader
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
 from llama_index.core.schema import Document
 from . import __path__
 
 
 def get_data(rdir: str = None) -> List[Document]:
+    """Load documents from a directory.
+
+    Parameters
+    ----------
+    rdir : str, optional, default is None.
+        Directory path
+
+    Returns
+    -------
+    List[Document]
+        List of documents
+    """
+
     if rdir is None:
         rdir = f"{__path__[0]}/data"
     documents = SimpleDirectoryReader(rdir).load_data()
     return documents
+
+
+def create_index(documents: List[Document]) -> VectorStoreIndex:
+    """Create an index from a list of documents.
+
+    Parameters
+    ----------
+    documents : List[Document]
+        List of documents
+
+    Returns
+    -------
+    VectorStoreIndex
+        VectorStoreIndex object
+    """
+
+    index = VectorStoreIndex.from_documents(
+        documents,
+    )
+    return index
