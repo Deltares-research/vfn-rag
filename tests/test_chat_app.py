9cb51edbe4d13c6000f105ffc555948077719873
Mostafa Farrag <moah.farag@gmail.com>
2025-04-13 00:48:47 +0200

    move un-needed modules to vfn-chat

--- tests/test_chat_app.py
+++
@@ -1,9 +0,0 @@
-from llm_rag.chat_model import get_data
-
-
-class TestGetData:
-
-    def test_default(self):
-        documents = get_data()
-        assert isinstance(documents, list)
-        assert len(documents) == 2
