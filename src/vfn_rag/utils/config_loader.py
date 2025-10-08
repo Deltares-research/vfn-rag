68283fbd2d4a2946c5c218af54a6ed2216f2244a
Marie-Lou David <marielou.David@deltares.nl>
2025-10-08 15:26:13 +0200

    custom node parser is available

--- src/vfn_rag/utils/config_loader.py
+++ src/vfn_rag/utils/config_loader.py
@@ -1,4 +1,5 @@
 from typing import Any
+from llama_index.core.node_parser import SentenceSplitter, TokenTextSplitter
 from llama_index.core import Settings
 from vfn_rag.utils.models import get_azure_open_ai_embedding, azure_open_ai
 
@@ -46,9 +47,29 @@ class ConfigLoader:
 
     @property
     def embedding(self):
-        return self._embedding
+        return self._embedding    
 
     @embedding.setter
     def embedding(self, value):
         self._embedding = value
         Settings.embed_model = value
+
+    def set_custom_node_parser(self, sentence_splitter: bool=True, chunk_size: int=512, chunk_overlap: int=20):
+        """Set a custom node parser for document splitting. (Has to be explicitly called when using a SimpleDirectoryReader)
+        Parameters
+        ----------
+        sentence_splitter: bool, optional, default=True
+            Whether to use SentenceSplitter or TokenTextSplitter.
+        chunk_size: int, optional, default=512
+            The size of each chunk.
+        chunk_overlap: int, optional, default=20
+            The overlap between chunks.
+        """
+        if sentence_splitter:
+            splitter = SentenceSplitter()
+        else:
+            splitter = TokenTextSplitter()
+        splitter.chunk_size = chunk_size
+        splitter.chunk_overlap = chunk_overlap
+        self.settings.node_parser = splitter
+        
