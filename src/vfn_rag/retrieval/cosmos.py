1fa81e3500e07f530826793b1b2c55d39f17bbcf
Mostafa Farrag <moah.farag@gmail.com>
2025-10-10 14:49:11 +0200

    create load and create functionalities in the `Cosmos` class

--- src/vfn_rag/retrieval/cosmos.py
+++ src/vfn_rag/retrieval/cosmos.py
@@ -41,6 +41,18 @@ INDEXING_POLICY = {
 __all__ = ["Cosmos"]
 
 
+def create_client(uri: str, key: str) -> CosmosClient:
+    """Create a CosmosClient instance."""
+    if uri is None:
+        uri = os.environ.get("AZURE_COSMOSDB_URI")
+    if key is None:
+        key = os.environ.get("AZURE_COSMOSDB_KEY")
+    if uri is None or key is None:
+        raise ValueError("Either cosmos_client or both uri and key must be provided")
+
+    return CosmosClient(uri, credential=key)
+
+
 class Cosmos(BaseStorage):
     """Factory to create a StorageContext using Azure Cosmos DB NoSQL.
 
@@ -75,22 +87,49 @@ class Cosmos(BaseStorage):
         client: Optional[CosmosClient] = None,
         uri: Optional[str] = None,
         key: Optional[str] = None,
-        indexing_policy: Optional[Dict[str, Any]] = None,
-        vector_embedding_policy: Optional[Dict[str, Any]] = None,
-        cosmos_container_properties: Optional[Dict[str, Any]] = None,
-        cosmos_database_properties: Optional[Dict[str, Any]] = None,
+        **kwargs: Any,
     ) -> "Cosmos":
         """Create and return a StorageContext configured with the Cosmos vector store."""
 
         if client is None:
-            if uri is None:
-                uri = os.environ.get("AZURE_COSMOSDB_URI")
-            if key is None:
-                key = os.environ.get("AZURE_COSMOSDB_KEY")
-            if uri is None or key is None:
-                raise ValueError("Either cosmos_client or both uri and key must be provided")
+            client = create_client(uri, key)
 
-            client = CosmosClient(uri, credential=key)
+        storage = cls._base_read_write(
+            database_name, container_name, client, create_container=True, **kwargs
+        )
+
+        return cls(storage, client)
+
+    @classmethod
+    def load(
+        cls,
+        database_name: str,
+        container_name: str,
+        client: Optional[CosmosClient] = None,
+        uri: Optional[str] = None,
+        key: Optional[str] = None,
+        **kwargs: Any,
+    ):
+        if client is None:
+            client = create_client(uri, key)
+
+        storage = cls._base_read_write(
+            database_name, container_name, client, create_container=False, **kwargs
+        )
+
+        return cls(storage, client)
+
+    @staticmethod
+    def _base_read_write(
+        database_name: str,
+        container_name: str,
+        client: Optional[CosmosClient],
+        create_container: bool = False,
+        indexing_policy: Optional[Dict[str, Any]] = None,
+        vector_embedding_policy: Optional[Dict[str, Any]] = None,
+        cosmos_container_properties: Optional[Dict[str, Any]] = None,
+        cosmos_database_properties: Optional[Dict[str, Any]] = None,
+    ) -> StorageContext:
 
         init_kwargs: Dict[str, Any] = {
             "cosmos_client": client,
@@ -100,13 +139,9 @@ class Cosmos(BaseStorage):
             "indexing_policy": indexing_policy or INDEXING_POLICY,
             "cosmos_container_properties": cosmos_container_properties or CONTAINER_PROPERTIES,
             "cosmos_database_properties": cosmos_database_properties or {},
-            "create_container": True,
+            "create_container": create_container,
         }
 
         store = AzureCosmosDBNoSqlVectorSearch(**init_kwargs)
         storage = StorageContext.from_defaults(vector_store=store)
-
-        return cls(storage, client)
-
-
-
+        return storage
