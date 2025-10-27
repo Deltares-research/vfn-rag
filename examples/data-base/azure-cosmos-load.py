13b0bf32ff1138abf67f5a9dd8e0c3c47e8afc79
Mostafa Farrag <moah.farag@gmail.com>
2025-10-27 11:49:38 +0100

    docs(storage): add Cosmos DB usage examples (#19)
    docs(storage): add Cosmos DB usage examples
    
    - Added and updated examples demonstrating Cosmos DB integration

--- examples/data-base/azure-cosmos-load.py
+++ examples/data-base/azure-cosmos-load.py
@@ -1,10 +1,8 @@
-#%% md
 # Azure Cosmos DB No SQL Vector Store
-# In this notebook we are going to show a quick demo of how to use AzureCosmosDBNoSqlVectorSearch to perform vector searches in LlamaIndex.
-# If you're opening this Notebook on colab, you will probably need to install LlamaIndex 🦙.
+# in this script we will load an existing vector store index from a data stored in cosmos db.
 #%%
 import os
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex
 from dotenv import load_dotenv
 from vfn_rag.utils.models import azure_open_ai, get_azure_open_ai_embedding
 from vfn_rag.utils.config_loader import ConfigLoader
@@ -15,20 +13,10 @@ load_dotenv()
 COSMOS_URI = os.getenv("AZURE_COSMOSDB_URI")
 COSMOS_KEY = os.getenv("AZURE_COSMOSDB_KEY")
 #%% Setup Azure OpenAI
-
 llm = azure_open_ai()
 embed_model = get_azure_open_ai_embedding()
 config = ConfigLoader(llm, embed_model)
-
-#%% Loading Documents
-# In this example we will be using the paul_graham essay which will be processed by the SimpleDirectoryReader.
-
-documents = SimpleDirectoryReader(
-    input_files=[r"examples/data/pond/deltares-pond-history.txt"]
-).load_data()
-
-print("Document ID:", documents[0].doc_id)
-#%% Create the index
+#%% connect to the cosmos db
 # Here we establish the connection to cosmos db nosql and create a vector store index.
 client = CosmosClient(COSMOS_URI, credential=COSMOS_KEY)
 database_name = "vectorSearchDB"
@@ -39,14 +27,17 @@ storage_context = Cosmos.load(
     database_name, container_name, client
 )
 store = storage_context.store
-#%% create index
-index = VectorStoreIndex.from_documents(
-    documents, storage_context=store
-)
-#%% md
-# Query the index
+#%% load index
+index = VectorStoreIndex.from_vector_store(vector_store=store.vector_store, embed_model=embed_model)
+
+#%% Query the index
 # We can now ask questions using our index.
-#%%
+# as a question answering engine
 query_engine = index.as_query_engine()
 response = query_engine.query("what is the history of the deltares pond?")
-print(response)
+print(response.response)
+
+# as a chat engine
+query_engine = index.as_chat_engine()
+response = query_engine.query("what is the history of the deltares pond?")
+print(response.response)
