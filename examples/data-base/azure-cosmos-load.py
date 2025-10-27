0c429fa43a89b0a6e62a1e55183d1f682ffd6271
Mostafa Farrag <moah.farag@gmail.com>
2025-10-27 11:45:01 +0100

    update examples

--- examples/data-base/azure-cosmos-load.py
+++ examples/data-base/azure-cosmos-load.py
@@ -1,8 +1,8 @@
 # Azure Cosmos DB No SQL Vector Store
-# in this script we will create a vector store index and store it in cosmos db nosql.
+# in this script we will load an existing vector store index from a data stored in cosmos db.
 #%%
 import os
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex
 from dotenv import load_dotenv
 from vfn_rag.utils.models import azure_open_ai, get_azure_open_ai_embedding
 from vfn_rag.utils.config_loader import ConfigLoader
@@ -13,19 +13,9 @@ load_dotenv()
 COSMOS_URI = os.getenv("AZURE_COSMOSDB_URI")
 COSMOS_KEY = os.getenv("AZURE_COSMOSDB_KEY")
 #%% Setup Azure OpenAI
-
 llm = azure_open_ai()
 embed_model = get_azure_open_ai_embedding()
 config = ConfigLoader(llm, embed_model)
-
-#%% Loading Documents
-# In this example we will be using the paul_graham essay which will be processed by the SimpleDirectoryReader.
-
-documents = SimpleDirectoryReader(
-    input_files=[r"examples/data/pond/deltares-pond-history.txt"]
-).load_data()
-
-print("Document ID:", documents[0].doc_id)
 #%% connect to the cosmos db
 # Here we establish the connection to cosmos db nosql and create a vector store index.
 client = CosmosClient(COSMOS_URI, credential=COSMOS_KEY)
@@ -37,12 +27,10 @@ storage_context = Cosmos.load(
     database_name, container_name, client
 )
 store = storage_context.store
-#%% create index
-index = VectorStoreIndex.from_documents(
-    documents, storage_context=store
-)
-#%% md
-# Query the index
+#%% load index
+index = VectorStoreIndex.from_vector_store(vector_store=store.vector_store, embed_model=embed_model)
+
+#%% Query the index
 # We can now ask questions using our index.
 #%%
 query_engine = index.as_query_engine()
